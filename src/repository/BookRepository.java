162e71be057fb87a6e825ebd795263252fe505cc
hayatoa0805-code <hayato.a.0805@gmail.com>
2026-09-02 11:09:09 +0900

    feat: BookRepository.javaを作成

---
+++ src/repository/BookRepository.java
@@ -0,0 +1,77 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.BookEntity;
+
+public class BookRepository {
+
+	private List<BookEntity> books = new ArrayList<>();
+
+	// 本の登録
+	public void save(int userId, BookEntity book) {
+		book.setUserId(userId);
+		books.add(book);
+	}
+
+	// ユーザー自身の登録してる本の一覧を取得
+	public List<BookEntity> findByUserId(int userId) {
+		List<BookEntity> result = new ArrayList<>();
+
+		for (BookEntity book : books) {
+			if (book.getUserId() == userId) {
+				result.add(book);
+			}
+		}
+
+		return result;
+	}
+
+	// ユーザー自身が登録してる本をタイトルで検索
+	public List<BookEntity> findByTitle(int userId, String title) {
+		List<BookEntity> result = new ArrayList<>();
+
+		for (BookEntity book : books) {
+			if (book.getUserId() == userId
+					&& book.getTitle().contains(title)) {
+				result.add(book);
+			}
+		}
+		return result;
+	}
+
+	// ユーザー自身が登録してる本を出版社で検索
+	public List<BookEntity> findByPublisher(int userId, String publisher) {
+		List<BookEntity> result = new ArrayList<>();
+
+		for (BookEntity book : books) {
+			if (book.getUserId() == userId
+					&& book.getPublisher().contains(publisher)) {
+				result.add(book);
+			}
+		}
+
+		return result;
+	}
+
+	// ユーザー自身が登録してる本を著者で検索
+	public List<BookEntity> findByAuthor(int userId, String author) {
+		List<BookEntity> result = new ArrayList<>();
+
+		for (BookEntity book : books) {
+			if (book.getUserId() == userId
+					&& book.getAuthor().contains(author)) {
+				result.add(book);
+			}
+		}
+
+		return result;
+	}
+
+	//　ユーザー自身が登録してる本を削除
+	public void delete(int userId, int id) {
+		books.removeIf(book -> book.getUserId() == userId &&
+				book.getId() == id);
+	}
+}
