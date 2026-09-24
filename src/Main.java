5d11a77b89148f674ec066f699280341b878312e
hayatoa0805-code <hayato.a.0805@gmail.com>
2026-09-24 23:20:44 +0900

    refactor: Main.javaのwhileをControllerに切り出し

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,11 @@
-import java.util.Scanner;
-
-import controller.BookController;
-import controller.UserController;
-import repository.BookRepository;
-import repository.UserRepository;
-import service.BookService;
-import service.UserService;
-import util.PasswordHasher;
-import view.BookView;
-import view.Menu;
-import view.UserView;
+import controller.AppController;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		boolean isBoot = true;
+		AppController appController = new AppController();
 
-		Scanner scan = new Scanner(System.in);
-		PasswordHasher passwordhasher = new PasswordHasher();
-
-		// User
-		UserRepository userRepository = new UserRepository();
-		UserService userService = new UserService(userRepository, passwordhasher);
-		UserController userController = new UserController(userService);
-
-		// Book
-		BookRepository bookRepository = new BookRepository();
-		BookService bookService = new BookService(bookRepository);
-		BookController bookController = new BookController(bookService);
-
-		// Menu
-		Menu menu = new Menu(scan);
-		UserView userView = new UserView(scan, menu, userController);
-
-		while (isBoot) {
-
-			// ログイン、新規登録
-			userView.showLoginMenu();
-
-			// ログイン後
-			while (userView.getLoginUser() != null) {
-
-				int userId = userView.getLoginUser().getUserId();
-
-				BookView bookView = new BookView(
-						scan,
-						menu,
-						bookController,
-						userId);
-
-				int choice = menu.mainMenu();
-				switch (choice) {
-
-				// User
-				case 1:
-					userView.showUserMenu();
-					break;
-
-				// Book
-				case 2:
-					bookView.showBookMenu();
-					break;
-
-				// アプリ終了
-				case 0:
-					System.out.println("アプリを終了します。");
-					isBoot = false;
-					return;
-
-				// 他の値が入力された場合
-				default:
-					System.out.print("正しい数字を入力してください。");
-					System.out.println();
-				}
-			}
-		}
+		appController.start();
 	}
 }
---
+++ src/controller/AppController.java
@@ -0,0 +1,86 @@
+package controller;
+
+import java.util.Scanner;
+
+import repository.BookRepository;
+import repository.UserRepository;
+import service.BookService;
+import service.UserService;
+import util.PasswordHasher;
+import view.BookView;
+import view.Menu;
+import view.UserView;
+
+public class AppController {
+
+	private Scanner scan;
+	private Menu menu;
+	private UserView userView;
+	private BookController bookController;
+
+	public AppController() {
+
+		scan = new Scanner(System.in);
+
+		PasswordHasher passwordHasher = new PasswordHasher();
+
+		// User
+		UserRepository userRepository = new UserRepository();
+		UserService userService = new UserService(userRepository, passwordHasher);
+		UserController userController = new UserController(userService);
+
+		// Book
+		BookRepository bookRepository = new BookRepository();
+		BookService bookService = new BookService(bookRepository);
+		bookController = new BookController(bookService);
+
+		// Menu
+		menu = new Menu(scan);
+		userView = new UserView(
+				scan,
+				menu,
+				userController);
+	}
+
+	public void start() {
+
+		boolean isBoot = true;
+
+		while (isBoot) {
+
+			userView.showLoginMenu();
+
+			while (userView.getLoginUser() != null) {
+
+				int userId = userView.getLoginUser().getUserId();
+
+				BookView bookView = new BookView(
+						scan,
+						menu,
+						bookController,
+						userId);
+
+				int choice = menu.mainMenu();
+
+				switch (choice) {
+
+				case 1:
+					userView.showUserMenu();
+					break;
+
+				case 2:
+					bookView.showBookMenu();
+					break;
+
+				case 0:
+					System.out.println("アプリを終了します。");
+					isBoot = false;
+					break;
+
+				default:
+					System.out.println("正しい数字を入力してください。");
+				}
+			}
+		}
+	}
+}
